e35fcd9245b2cdd852017d136ee37fe5e9324cda
YolxanderHC <yolxander.jaca@humancontact.com>
2024-10-08 16:11:58 -0400

    fixed erros part 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Audiowide, Roboto_Condensed, Poppins } from "@next/font/google";
+import { Audiowide, Roboto_Condensed } from "@next/font/google";
 import "./globals.css";
 import Script from 'next/script'
 
