654ab7b13ca376c351e32b3a5a0c19a03c8a01b0
YolxanderHC <yolxander.jaca@humancontact.com>
2024-11-12 21:05:35 -0500

    updated feature text

--- src/components/landing-page/text-section.tsx
+++ src/components/landing-page/text-section.tsx
@@ -30,7 +30,7 @@ export function TextSectionComponent() {
             <span className="w-2 h-2 bg-black rounded-full animate-pulse"></span>
             <span>how it works</span>
           </span>
-          <span className="font-bold pb-5">
+          <span className="font-bold pb-3">
             With Sempre Studios {" "}
           </span>
         </span>
@@ -40,7 +40,7 @@ export function TextSectionComponent() {
             custom={index}
             animate={controls}
             style={{ color: "#D1D5DB" }}
-            className={animationComplete ? "text-black" : ""}
+            className={animationComplete ? "text-black leading-loose" : ""}
           >
             {letter}
           </motion.span>
